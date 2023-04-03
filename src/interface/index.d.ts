3edc45670951dae9ee63c9b6988d61d1e9fc648f
karlfranz <2031665973@qq.com>
2023-04-03 20:34:37 +0800

    [尝试修改总访问量问题2]

--- src/interface/index.d.ts
+++ src/interface/index.d.ts
@@ -2,3 +2,8 @@ declare module "*.js" {
   import busuanzi from "busuanzi.pure.js";
   export default busuanzi;
 }
+
+declare module "busuanzi" {
+  import busuanzi from "busuanzi";
+  export default busuanzi;
+}
